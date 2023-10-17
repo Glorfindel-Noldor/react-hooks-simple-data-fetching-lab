a0b5020802a7fb02bde1e4c7442f810b66424557
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-10-16 22:03:04 -0500

    i love coffee and react with side Effects and a side of fetch with extra coffee

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+const API = "https://dog.ceo/api/breeds/image/random";
+
+function App() {
+  const [intel, setIntel] = useState({
+    load: false,
+    picture: "",
+  });
+
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) =>
+        setIntel({
+          load: true,
+          picture: data.message,
+        })
+      );
+  }, []);
+
+  return (
+    <React.Fragment>
+      {intel.load === false ? 
+      (<p>Loading...</p>)
+      : 
+      (<div>
+          <img src={intel.picture} alt="A Random Dog" />
+        </div>)
+      }
+    </React.Fragment>
+  )
+}
+
+export default App;
